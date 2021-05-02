24b8fcf5d45599dbedc48f468c6cf15371bf6edb
unknown <Adrian Stawarz>
2021-05-02 21:43:33 +0200

    odd/even

---
+++ src/CheckOddEven.java
@@ -0,0 +1,9 @@
+public class CheckOddEven {
+    public void checkOddEven(int num) {
+        if (num % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 import java.lang.Integer;
 public class Main {
     public static void main(String[] args) {
+
         Maximum maxx = new Maximum();
         Integer[] numList = {22, 100, 41, -5, 555, -22222, 33};
         System.out.println(maxx.maximum(numList));
+
         NumberInWord numberInWord = new NumberInWord();
         System.out.println(numberInWord.printNumberInWord(9));
         System.out.println(numberInWord.printNumberInWord(8));
         System.out.println(numberInWord.printNumberInWord(7));
+
+        CheckOddEven checkOddEven = new CheckOddEven();
+        checkOddEven.checkOddEven(9);
+        checkOddEven.checkOddEven(8);
+        checkOddEven.checkOddEven(22);
+        checkOddEven.checkOddEven(444);
+        checkOddEven.checkOddEven(333);
 
     }
 }
